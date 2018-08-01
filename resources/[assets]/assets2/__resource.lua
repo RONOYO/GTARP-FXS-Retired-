8c41fe003a62838ca884de98c3f6194bcbd48a61
BuildTools <unconfigured@null.spigotmc.org>
2018-07-31 21:16:10 -0700

    Added SASP Shit and fixed vMenu. I think.....

--- resources/[assets]/assets2/__resource.lua
+++ resources/[assets]/assets2/__resource.lua
@@ -9,5 +9,3 @@ files {
 data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
 data_file 'CARCOLS_FILE' 'data/carcols.meta'
 data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
-
-client_script 'data/vehicle_names.lua'
