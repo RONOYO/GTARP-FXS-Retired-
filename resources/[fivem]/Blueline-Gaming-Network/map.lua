df977a67dc1a9f5d285c35263ee5d3891e9b2862
BLGN VPS <ronan-young@users.noreply.github.com>
2018-08-01 00:42:04 -0700

    added some shot

---
+++ resources/[fivem]/Blueline-Gaming-Network/map.lua
@@ -0,0 +1,7 @@
+-- Blaine County Spawn
+spawnpoint 'a_f_m_salton_01' { x = 734.44116210938, y = 2523.4282226563, z = 73.226020812988 }
+spawnpoint 'u_m_y_hippie_01' { x = 734.44116210938, y = 2523.4282226563, z = 73.226020812988 }
+
+-- Paleto Bay Spawn
+spawnpoint 'a_f_m_salton_01' { x = -219.58715820313, y = 6561.2368164063, z = 10.958762168884 }
+spawnpoint 'u_m_y_hippie_01' { x = -219.58715820313, y = 6561.2368164063, z = 10.958762168884 }
--- resources/[scripts]/realism/client.lua
+++ resources/[scripts]/realism/client.lua
@@ -641,7 +641,7 @@ end)
 					ClearPedTasks(ped)
 					holstered = false
 				end
-				SetPedComponentVariation(ped, 9, 0, 0, 0)
+				
 			elseif not CheckWeapon(ped) then
 				if not holstered then
 					TaskPlayAnim(ped, "weapons@pistol@", "aim_2_holster", 20.0, 0.4, -1, 50, 0, false, false, false)
@@ -649,7 +649,6 @@ end)
 					ClearPedTasks(ped)
 					holstered = true
 				end
-				SetPedComponentVariation(ped, 9, 1, 0, 0)
 			end
 		end
 	end
@@ -692,5 +691,119 @@ function loadAnimDict( dict )
 	while ( not HasAnimDictLoaded( dict ) ) do
 		RequestAnimDict( dict )
 		Citizen.Wait( 0 )
+	end
+end
+
+local blips = {
+
+     {title="Spawn Point", colour=0, id=40, x = 734.44116210938, y = 2523.4282226563, z = 73.226020812988},
+	 {title="Spawn Point", colour=0, id=40, x = -219.58715820313, y = 6561.2368164063, z = 10.958762168884},
+  }
+      
+Citizen.CreateThread(function()
+
+    for _, info in pairs(blips) do
+      info.blip = AddBlipForCoord(info.x, info.y, info.z)
+      SetBlipSprite(info.blip, info.id)
+      SetBlipDisplay(info.blip, 4)
+      SetBlipScale(info.blip, 0.8)
+      SetBlipColour(info.blip, info.colour)
+      SetBlipAsShortRange(info.blip, true)
+	  BeginTextCommandSetBlipName("STRING")
+      AddTextComponentString(info.title)
+      EndTextCommandSetBlipName(info.blip)
+    end
+end)
+
+
+local crouched = false
+local proned = false
+crouchKey = 26
+proneKey = 36
+
+Citizen.CreateThread( function()
+	while true do 
+		Citizen.Wait( 1 )
+		local ped = GetPlayerPed( -1 )
+		if ( DoesEntityExist( ped ) and not IsEntityDead( ped ) ) then 
+			ProneMovement()
+			DisableControlAction( 0, proneKey, true ) 
+			DisableControlAction( 0, crouchKey, true ) 
+			if ( not IsPauseMenuActive() ) then 
+				if ( IsDisabledControlJustPressed( 0, crouchKey ) and not proned and not IsPedInAnyVehicle(ped, true)) then 
+					RequestAnimSet( "move_ped_crouched" )
+					
+					while ( not HasAnimSetLoaded( "move_ped_crouched" ) ) do 
+						Citizen.Wait( 100 )
+					end 		
+					if ( crouched and not proned ) then 
+						ResetPedMovementClipset( ped )
+						ResetPedStrafeClipset(ped)
+						crouched = false 
+					elseif ( not crouched and not proned ) then
+						SetPedMovementClipset( ped, "move_ped_crouched", 0.55 )
+						SetPedStrafeClipset(ped, "move_ped_crouched_strafing")
+						crouched = true 
+					end 
+				elseif ( IsDisabledControlJustPressed(0, proneKey) and not crouched and not IsPedInAnyVehicle(ped, true) and not IsPedFalling(ped) and not IsPedDiving(ped) and not IsPedInCover(ped, false) and not IsPedInParachuteFreeFall(ped) and (GetPedParachuteState(ped) == 0 or GetPedParachuteState(ped) == -1) ) then
+					if proned then
+						ClearPedTasksImmediately(ped)
+						proned = false
+					elseif not proned then
+						RequestAnimSet( "move_crawl" )
+						while ( not HasAnimSetLoaded( "move_crawl" ) ) do 
+							Citizen.Wait( 100 )
+						end 
+						ClearPedTasksImmediately(ped)
+						proned = true
+						if IsPedSprinting(ped) or IsPedRunning(ped) or GetEntitySpeed(ped) > 5 then
+							TaskPlayAnim(ped, "move_jump", "dive_start_run", 8.0, 1.0, -1, 0, 0.0, 0, 0, 0)
+							Citizen.Wait(1000)
+						end
+						SetProned()
+					end
+				end
+			end
+		else
+			proned = false
+			crouched = false
+		end
+	end
+end)
+
+function SetProned()
+	ped = PlayerPedId()
+	ClearPedTasksImmediately(ped)
+	TaskPlayAnimAdvanced(ped, "move_crawl", "onfront_fwd", GetEntityCoords(ped), 0.0, 0.0, GetEntityHeading(ped), 1.0, 1.0, 1.0, 46, 1.0, 0, 0)
+end
+
+
+function ProneMovement()
+	if proned then
+		ped = PlayerPedId()
+		if IsControlPressed(0, 32) or IsControlPressed(0, 33) then
+			DisablePlayerFiring(ped, true)
+		 elseif IsControlJustReleased(0, 32) or IsControlJustReleased(0, 33) then
+		 	DisablePlayerFiring(ped, false)
+		 end
+		if IsControlJustPressed(0, 32) and not movefwd then
+			movefwd = true
+		    TaskPlayAnimAdvanced(ped, "move_crawl", "onfront_fwd", GetEntityCoords(ped), 1.0, 0.0, GetEntityHeading(ped), 1.0, 1.0, 1.0, 47, 1.0, 0, 0)
+		elseif IsControlJustReleased(0, 32) and movefwd then
+		    TaskPlayAnimAdvanced(ped, "move_crawl", "onfront_fwd", GetEntityCoords(ped), 1.0, 0.0, GetEntityHeading(ped), 1.0, 1.0, 1.0, 46, 1.0, 0, 0)
+			movefwd = false
+		end		
+		if IsControlJustPressed(0, 33) and not movebwd then
+			movebwd = true
+		    TaskPlayAnimAdvanced(ped, "move_crawl", "onfront_bwd", GetEntityCoords(ped), 1.0, 0.0, GetEntityHeading(ped), 1.0, 1.0, 1.0, 47, 1.0, 0, 0)
+		elseif IsControlJustReleased(0, 33) and movebwd then 
+		    TaskPlayAnimAdvanced(ped, "move_crawl", "onfront_bwd", GetEntityCoords(ped), 1.0, 0.0, GetEntityHeading(ped), 1.0, 1.0, 1.0, 46, 1.0, 0, 0)
+		    movebwd = false
+		end
+		if IsControlPressed(0, 34) then
+			SetEntityHeading(ped, GetEntityHeading(ped)+2.0 )
+		elseif IsControlPressed(0, 35) then
+			SetEntityHeading(ped, GetEntityHeading(ped)-2.0 )
+		end
 	end
 end
